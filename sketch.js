879fed50631836b7b9d00ccb7241bfed6d80f0e4
caio <pires.goncalves.caio@escola.pr.gov.br>
2024-08-12 20:41:33 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 400);
+   background("red");
+  }
+  function draw() {
+   
+    stroke("purple");
+    fill("black");
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
